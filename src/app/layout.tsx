8fd65593f0fe2bcb6c198497329408557302cb7a
HJK <danyil.butov@gmail.com>
2024-11-11 12:20:06 +0000

    fix: change theme-colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: light)', color: 'hsl(0, 0%, 100%)' },
+    { media: '(prefers-color-scheme: dark)', color: 'hsl(0, 0%, 7%)' },
   ],
 };
 
