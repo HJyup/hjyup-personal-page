d8c11b8981289c326deb55748d335e5574e4a681
HJK <danyil.butov@gmail.com>
2025-06-21 02:32:04 +0100

    fix: reorder widgets

--- src/const/widgets.ts
+++ src/const/widgets.ts
@@ -14,20 +14,20 @@ export type WidgetComponentType =
   | 'projects-logo-card';
 
 export const DEFAULT_WIDGETS: Widget[] = [
-  { id: 'notes-card', component: 'notes-card', column: 0, order: 0 },
-  { id: 'github-card', component: 'github-card', column: 0, order: 1 },
+  { id: 'notes-card', component: 'notes-card', column: 1, order: 0 },
+  { id: 'github-card', component: 'github-card', column: 0, order: 2 },
   {
     id: 'projects-logo-card',
     component: 'projects-logo-card',
     column: 0,
-    order: 2,
+    order: 0,
   },
-  { id: 'project-card', component: 'project-card', column: 1, order: 3 },
+  { id: 'project-card', component: 'project-card', column: 0, order: 1 },
   { id: 'music-card', component: 'music-card', column: 1, order: 2 },
   {
     id: 'photo-social-container',
     component: 'photo-social-container',
     column: 1,
-    order: 2,
+    order: 3,
   },
 ];
