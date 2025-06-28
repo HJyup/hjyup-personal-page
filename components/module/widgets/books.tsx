9c1b9f1490378e31e61c5e62099d42c676ed3c61
HJK <danyil.butov@gmail.com>
2025-06-28 14:42:15 +0100

    fix: fix and polish logos and images

--- components/module/widgets/books.tsx
+++ components/module/widgets/books.tsx
@@ -16,6 +16,7 @@ export function BooksWidget({ className = '' }: { className?: string }) {
           src="/books/design_of_things.jpg"
           alt="The Design of Everyday Things"
           width={288}
+          draggable={false}
           height={432}
         />
 
@@ -25,9 +26,12 @@ export function BooksWidget({ className = '' }: { className?: string }) {
             <div className="text-zinc-500 dark:text-zinc-400 text-xs sm:text-sm">
               CURRENTLY READING
             </div>
-            <h3 className="text-zinc-700 dark:text-zinc-300 font-medium text-base sm:text-lg leading-tight">
+            <a
+              href="https://www.amazon.co.uk/Design-Everyday-Things-Revised-Expanded/dp/0465050654/ref=asc_df_0465050654?mcid=e93f5bb3984336ac97c5ef83aea9e26c&hvocijid=9856961943512471641-0465050654-&hvexpln=74&tag=googshopuk-21&linkCode=df0&hvadid=696285193871&hvpos=&hvnetw=g&hvrand=9856961943512471641&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9045438&hvtargid=pla-2281435176898&psc=1&gad_source=1"
+              className="text-zinc-700 dark:text-zinc-300 font-medium text-base sm:text-lg leading-tight hover:underline"
+            >
               The Design of Everyday Things
-            </h3>
+            </a>
             <p className="text-zinc-400 dark:text-zinc-600 text-xs sm:text-sm">
               by Don Norman
             </p>
