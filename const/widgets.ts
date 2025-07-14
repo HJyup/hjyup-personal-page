5b78a36c2d2b45786e781ae2fd3698699cae806f
HJK <danyil.butov@gmail.com>
2025-07-14 10:43:22 +0100

    fix : widget position

--- const/widgets.ts
+++ const/widgets.ts
@@ -18,14 +18,14 @@ export type WidgetComponentType =
 
 export const DEFAULT_WIDGETS: Widget[] = [
   // { id: 'notes-card', component: 'notes-card', column: 1, order: 0 },
-  { id: 'github-card', component: 'github-card', column: 1, order: 1 },
+  { id: 'github-card', component: 'github-card', column: 0, order: 1 },
   { id: 'books-card', component: 'books-card', column: 0, order: 3 },
-  { id: 'email-card', component: 'email-card', column: 0, order: 1 },
+  { id: 'email-card', component: 'email-card', column: 1, order: 0 },
   // { id: 'contact-card', component: 'contact-card', column: 1, order: 4 },
   {
     id: 'projects-logo-card',
     component: 'projects-logo-card',
-    column: 1,
+    column: 0,
     order: 0,
   },
   { id: 'music-card', component: 'music-card', column: 1, order: 2 },
